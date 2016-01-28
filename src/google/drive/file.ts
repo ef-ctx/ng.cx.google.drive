32ef116b40963a3cc932de6daa0ce8ffc3f0f610
Fredrik Bonander <carl.fredrik.bonander@gmail.com>
2016-01-28 14:57:34 +0000

    fix: adds VideoMediaMetadata to FileResource

--- src/google/drive/file.ts
+++ src/google/drive/file.ts
@@ -3,6 +3,24 @@ const MIME_TYPES = {
 	'FOLDER': 'application/vnd.google-apps.folder'
 }
 
+interface IVideoMediaMetadata {
+	width: number
+	height: number
+	durationMillis: number
+}
+
+class VideoMediaMetadata implements IVideoMediaMetadata {
+	width: number
+	height: number
+	durationMillis: number
+
+	constructor(data: IVideoMediaMetadata) {
+		this.width = data && data.width;
+		this.height = data && data.height;
+		this.durationMillis = data && data.durationMillis;
+	}
+}
+
 export class DriveFileResource {
 	kind: string
   id: string
@@ -19,6 +37,7 @@ export class DriveFileResource {
   thumbnailLink: string
   createdTime: Date
   modifiedTime: Date
+  videoMediaMetadata: VideoMediaMetadata
 
 	constructor(data:any) {
 		this.kind = data.kind;
@@ -36,6 +55,7 @@ export class DriveFileResource {
 		this.thumbnailLink = data.thumbnailLink;
 		this.createdTime = data.createdTime;
 		this.modifiedTime = data.modifiedTime;
+		this.videoMediaMetadata = new VideoMediaMetadata(data.videoMediaMetadata);
 	}
 
 	isFolder():boolean {
