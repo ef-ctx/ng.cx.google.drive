21fdcb198dd5112aa1b1d671a440cf38dfae3cb6
Fredrik Bonander <carl.fredrik.bonander@gmail.com>
2016-02-02 17:24:25 +0000

    fix: use this over return

--- src/cx/googleDrive.ts
+++ src/cx/googleDrive.ts
@@ -6,21 +6,19 @@ export declare var angular: any;
 
 export class GoogleDrive {
     static bootstrap() {
-			API.bootstrap('drive');
+      API.bootstrap('drive');
 
       angular.module('ng.cx.google.drive', [
-			])
-				.service('google', [
-					function() {
-						'use strict';
+      ])
+        .service('google', [
+          function() {
+            'use strict';
 
-						return {
-							'api': API,
-							'Auth': Auth,
-							'drive': Drive,
-							'DriveQuery': DriveQuery
-						};
-					}
-				]);
+            this.api = API;
+            this.Auth = Auth;
+            this.drive = Drive;
+            this.DriveQuery = DriveQuery;
+          }
+        ]);
     }
 }
