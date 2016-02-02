febe395f301733ff68abef1938b9ea81b80c7f8b
Fredrik Bonander <carl.fredrik.bonander@gmail.com>
2016-02-02 17:34:03 +0000

    fix: bundle config

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -2,16 +2,21 @@
 	'use strict';
 
 	var gulp = require('gulp'),
-	Builder = require('systemjs-builder');
+	  concat = require('gulp-concat'),
+    Builder = require('systemjs-builder');
 
 	function dist(appConfig) {
 		return function (cb) {
 			var builder = new Builder(appConfig.output, appConfig.typescript.systemConfig),
 				inputPath = appConfig.typescript.inputPath,
-				outputFile = appConfig.dist + 'ng.cx.google.drive.js';;
+				outputFile = appConfig.dist + 'ng.cx.google.drive.js';
 
 				builder.bundle(inputPath, outputFile,  { minify: true, sourceMaps: true })
 					.then(function () {
+            gulp.src([outputFile, appConfig.typescript.systemConfig])
+              .pipe(concat('ng.cx.google.drive.js'))
+              .pipe(gulp.dest(appConfig.dist));
+
 						cb();
 					})
 					.catch(function (ex) {
