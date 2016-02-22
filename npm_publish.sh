3987a869fcb47ea19d2f5f1e951c2a130d8dbb7f
Fredrik Bonander <carl.fredrik.bonander@gmail.com>
2016-02-22 13:37:19 +0100

    feat: npm publish script

---
+++ npm_publish.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+NPM_DIR='dist'
+NPM_BUMP='patch'
+BUMPS=('major' 'minor' 'patch')
+
+while test $# -gt 0; do
+  case "$1" in
+    -b*|--bump*)
+      NPM_BUMP=`echo $1 | sed -e 's/^[^=]*=//g'`
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if [[ " ${arr[*]} " == *" $NPM_BUMP "* ]]; then
+    echo "arr contains d"
+fi
+
+validBump () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+validBump $NPM_BUMP "${BUMPS[@]}"
+if [ $? != 0 ]; then
+  echo "$NPM_BUMP is not a vaild bump string, use major, minor, patch"
+  exit 1
+fi
+
+rm -fr $NPM_DIR
+gulp dist
+
+npm version $NPM_BUMP -m "chore(release): %s"
+
+cp package.json $NPM_DIR
+npm publish $NPM_DIR
+git push --tags
